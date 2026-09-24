def3a93119e8b7f3a2394c8d63ee54a550a5535a
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-24 12:21:46 +0500

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,2 @@
-// Architectural pattern: MVC, Dependency Injection, MVP
-// MVC = MODEL VIEW CONTROLLER
-
-console.log("Hello")
-// Design pattern: Middleware, Decorator
+import dotenv from "dotenv";
+dotenv.config();
