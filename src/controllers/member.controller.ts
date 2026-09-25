3be821939ad77f7576ec103c5a77082df9bf2c7e
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-25 07:38:58 +0500

    fix: modify routing system and controllers

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,28 +2,5 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 
 const memberController: T = {};
-memberController.goHome = (req: Request, res: Response) => {
-  try {
-    res.send("Home Page");
-  } catch (err) {
-    console.log("Error, go home", err);
-  }
-};
-
-memberController.getLogin = (req: Request, res: Response) => {
-  try {
-    res.send("Login Page");
-  } catch (err) {
-    console.log("Error, getLogin", err);
-  }
-};
-
-memberController.getSignup = (req: Request, res: Response) => {
-  try {
-    res.send("Signup Page");
-  } catch (err) {
-    console.log("Error, getSignup", err);
-  }
-};
 
 export default memberController;
